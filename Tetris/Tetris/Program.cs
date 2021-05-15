01bfc5127d6d92f504041efcff0e633777b0ff8a
Necmic <ohohoh@bk.ru>
2021-05-15 22:27:34 +0300

    Добавлен класс результатов Result и сама проверка столкновений, как с краями, так и с кучей точек игрового поля.

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -5,16 +5,14 @@ namespace Tetris
 {
     class Program
     {
+        static FigureGenerator generator;
         static void Main(string[] args)
         {
             Console.SetWindowSize(Field.Width, Field.Height);
             Console.SetBufferSize(Field.Width, Field.Height);
             Console.CursorVisible = false;
 
-            //Field.Width = 20;
-            //Field.Height = 25;
-
-            FigureGenerator generator = new FigureGenerator(Console.BufferWidth / 2, 0, '*');
+            generator = new FigureGenerator(Field.Width / 2, 0, '*');
             Figure currentFigure = generator.GetNewFigure();
 
             while (true)
@@ -22,28 +20,38 @@ namespace Tetris
                 if (Console.KeyAvailable)
                 {
                     var key = Console.ReadKey();
-                    HandleKey(currentFigure, key);
+                    var result = HandleKey(currentFigure, key.Key);
+                    ProcessResult(result, ref currentFigure);
                 }
             }
         }
-
-        private static void HandleKey(Figure currentFigure, ConsoleKeyInfo key)
+        
+        private static bool ProcessResult(Result result, ref Figure currentFigure)
         {
-            switch (key.Key)
+            if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
+            {
+                Field.AddFigure(currentFigure);
+                currentFigure = generator.GetNewFigure();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private static Result HandleKey(Figure f, ConsoleKey key)
+        {
+            switch (key)
             {
                 case ConsoleKey.LeftArrow:
-                    currentFigure.TryMove(Direction.LEFT);
-                    break;
+                    return f.TryMove(Direction.LEFT);
                 case ConsoleKey.RightArrow:
-                    currentFigure.TryMove(Direction.RIGHT);
-                    break;
+                    return f.TryMove(Direction.RIGHT);
                 case ConsoleKey.DownArrow:
-                    currentFigure.TryMove(Direction.DOWN);
-                    break;
+                    return f.TryMove(Direction.DOWN);
                 case ConsoleKey.Spacebar:
-                    currentFigure.TryRotate();
-                    break;
+                    return f.TryRotate();
             }
+            return Result.SUCCESS;
         }
 
         //static void FigureFall(out Figure fig, FigureGenerator gen)
