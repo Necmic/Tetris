40d0ab67f9eed3dd58fd38b6bba3dc7416498b1d
Necmic <ohohoh@bk.ru>
2021-05-17 06:33:02 +0300

    Сделан проект TetrisGui на графической библиотеке SmallBasic. Но всё виснет при многопоточной работе, не смотря на критическую секцию на базе Monitor.Enter()/Exit().

---
+++ Tetris/TetrisGui/Figure.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris
+{
+    abstract class Figure
+    {
+        const int LENGTH = 4;
+        public Point[] Points = new Point[LENGTH];
+
+        public void Draw()
+        {
+            foreach (Point p in Points)
+            {
+                p.Draw();
+            }
+        }
+
+        public Result TryMove(Direction dir, bool t = false)
+        {
+            if (t) Console.WriteLine("Timer_1_0");
+            Hide();
+            if (t) Console.WriteLine("Timer_1_1");
+            Move(dir);
+            if (t) Console.WriteLine("Timer_1_2");
+
+            var result = VerifyPosition();
+            if (t) Console.WriteLine("Timer_1_3");
+            if (result != Result.SUCCESS)
+                Move(Reverse(dir));
+            if (t) Console.WriteLine("Timer_1_4");
+
+            Draw();
+            if (t) Console.WriteLine("Timer_1_5");
+            return result;
+        }
+
+        public Result TryRotate()
+        {
+            Hide();
+            Rotate();
+
+            var result = VerifyPosition();
+            if (result != Result.SUCCESS)
+                Rotate();
+
+            Draw();
+            return result;
+        }
+
+        private Result VerifyPosition()
+        {
+            foreach (var p in Points)
+            {
+                if (p.Y >= Field.Height)
+                    return Result.DOWN_BORDER_STRIKE;
+
+                if (p.X >= Field.Width || p.X < 0 || p.Y < 0)
+                    return Result.BORDER_STRIKE;
+
+                if (Field.CheckStrike(p))
+                    return Result.HEAP_STRIKE;
+            }
+            return Result.SUCCESS;
+        }
+
+        public void Move(Direction dir)
+        {
+            foreach (var p in Points)
+            {
+                p.Move(dir);
+            }
+        }
+
+        private Direction Reverse(Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.LEFT:
+                    return Direction.RIGHT;
+                case Direction.RIGHT:
+                    return Direction.LEFT;
+                case Direction.DOWN:
+                    return Direction.UP;
+                case Direction.UP:
+                    return Direction.DOWN;
+            }
+            return dir;
+        }
+
+        internal bool IsOnTop()
+        {
+            return Points[0].Y == 0;
+        }
+
+        //public void TryMove(Direction dir)
+        //{
+        //    // Моя реализация контроля границ окна
+        //    int w = Console.BufferWidth - 1;
+        //    int h = Console.BufferHeight - 1;
+        //    foreach (Point p in points)
+        //    {
+        //        switch (dir)
+        //        {
+        //            case Direction.LEFT:
+        //                if (p.X <= 0)
+        //                    return;
+        //                break;
+        //            case Direction.RIGHT:
+        //                if (p.X >= w)
+        //                    return;
+        //                break;
+        //            case Direction.DOWN:
+        //                if (p.Y >= h)
+        //                    return;
+        //                break;
+        //        }
+        //    }
+        //    Hide();
+        //    foreach (Point p in points)
+        //    {
+        //        p.Move(dir);
+        //    }
+        //    Draw();
+        //}
+
+        public abstract void Rotate();
+
+        public void Hide()
+        {
+            foreach (Point p in Points)
+            {
+                p.Hide();
+            }
+        }
+
+    }
+}
---
+++ Tetris/TetrisGui/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using Microsoft.SmallBasic.Library;
+
+// !!!!! Херово работает и виснет - какой-то конфликт в библиотете SmartBasic
+//       Наверное она не поддерживает многопоточность ???
+
+// - зависает в функции OnTimedEvent() здесь:
+//     var result = currentFigure.TryMove(Direction.DOWN);
+//     - из функции не выходит никогда, если в момент обработки была нажата кнопка и
+//       произошёл вход в функцию обратного вызова GraphicsWindow_KeyDown(), которая похоже
+//       плохо поддерживает стандартную многопоточность... Monitor.Enter(object)
+//       - зависание происходит при вызове функции Monitor.Enter(object) в обработчике кнопок?
+// https://docs.microsoft.com/ru-ru/dotnet/api/system.threading.monitor.enter?view=netframework-4.7.2
+// подвисание происходит в:
+// - Hide() если была нажата кнопка
+// - Draw() если была нажата кнопка
+// gameOver = ProcessResult(result, ref currentFigure);
+
+
+namespace Tetris
+{
+    class Program
+    {
+        const int TIMER_INTERVAL = 500;
+        static System.Timers.Timer timer;
+        static Object lockObject = new object();
+
+        static bool gameOver = false;
+        static Figure currentFigure;
+        static FigureGenerator generator = new FigureGenerator(Field.Width / 2, 0);
+
+        static void Main(string[] args)
+        {
+            DrawerProvider.Drawer.InitField();
+            currentFigure = generator.GetNewFigure();
+            SetTimer();
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            Console.WriteLine("Key_0");
+            Monitor.Enter(lockObject);
+            Console.WriteLine("Key_1");
+            var result = HandleKey(currentFigure, GraphicsWindow.LastKey);
+            if (GraphicsWindow.LastKey == "Down")
+                gameOver = ProcessResult(result, ref currentFigure);
+            Console.WriteLine("Key_2");
+            Monitor.Exit(lockObject);
+            Console.WriteLine("Key_3");
+        }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            timer = new System.Timers.Timer(TIMER_INTERVAL);
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Timer_0");
+            Monitor.Enter(lockObject);
+            Console.WriteLine("Timer_1");
+            var result = currentFigure.TryMove(Direction.DOWN, true);
+            Console.WriteLine("Timer_11");
+            gameOver = ProcessResult(result, ref currentFigure);
+            Console.WriteLine("Timer_12");
+            if (gameOver)
+                timer.Stop();
+            Console.WriteLine("Timer_2");
+            Monitor.Exit(lockObject);
+            Console.WriteLine("Timer_3");
+        }
+
+        private static bool ProcessResult(Result result, ref Figure currentFigure)
+        {
+            if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
+            {
+                Field.AddFigure(currentFigure);
+                Field.TryDeleteLines();
+
+                if (currentFigure.IsOnTop())
+                {
+                    // timer.Elapsed -= OnTimedEvent;
+                    DrawerProvider.Drawer.WriteGameOver();
+                    return true;
+                }
+                else
+                {
+                    currentFigure = generator.GetNewFigure();
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+
+        private static Result HandleKey(Figure f, String key)
+        {
+            switch (key)
+            {
+                case "Left":
+                    return f.TryMove(Direction.LEFT);
+                case "Right":
+                    return f.TryMove(Direction.RIGHT);
+                case "Down":
+                    return f.TryMove(Direction.DOWN);
+                case "Space":
+                    return f.TryRotate();
+            }
+            return Result.SUCCESS;
+        }
+    }
+}
